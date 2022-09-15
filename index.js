0dbe53611cf984566f31f1d96bb6d47084d286de
Glitch (codingtrain-the-weather-here) <none>
2022-09-15 12:03:50 +0000

    🎟🎫 Checkpoint ./index.js:48737440/3323 ./public/sketch.js:48737440/383

--- index.js
+++ index.js
@@ -23,35 +23,24 @@ app.post('/hello', (request, response) => {
   response.json(data);
 });
 
+app.get('/replicate_api/:prompt', async (request, response) => {
 
-curl -s -X POST \
-  -d '{"version": "29565a19f1fb04c8d8e9864bc08a22ce0c982398165fa4cdc61d2f2c777a35a0", "input": {"prompt": "female cyborg assimilated by alien fungus, intricate Three-point lighting portrait, by Ching Yeh and Greg Rutkowski, detailed cyberpunk in the style of GitS 1995"}}' \
-  -H "Authorization: Token $REPLICATE_API_TOKEN" \
-  -H 'Content-Type: application/json' \
-  https://api.replicate.com/v1/predictions
-app.get('/replicate_api/:latlon', async (request, response) => {
-  console.log(request.params);
-  const latlon = request.params.latlon.split(',');
-  console.log(latlon);
-  const lat = latlon[0];
-  const lon = latlon[1];
-  console.log(lat, lon);
-  const api_key = process.env.API_KEY;
-  const weather_url = `https://api.replicate.com/v1/predictions${api_key}/${lat},${lon}/?units=si`;
-  const weather_response = await fetch(weather_url);
-  const weather_data = await weather_response.json();
-
-  const aq_url = `https://api.openaq.org/v1/latest?coordinates=${lat},${lon}`;
-  const aq_response = await fetch(aq_url);
-  const aq_data = await aq_response.json();
-
-  const data = {
-    weather: weather_data,
-    air_quality: aq_data
-  };
-  response.json(data);
-});
-
+async function query(extra_data) {
+   let data = {"version": "29565a19f1fb04c8d8e9864bc08a22ce0c982398165fa4cdc61d2f2c777a35a0", "input": {"prompt": "female cyborg assimilated by alien fungus, intricate Three-point lighting portrait, by Ching Yeh and Greg Rutkowski, detailed cyberpunk in the style of GitS 1995"}};
+   console.log(prompt + " but hardwired for now");
+  const response = await fetch(
+     
+    // 'https://api-inference.huggingface.co/models/EleutherAI/gpt-neo-125M',
+    'https://api.replicate.com/v1/predictions',
+    {
+      headers: { Authorization: `Bearer ${process.env.REPLICATE_API_KEY}` },
+      method: 'POST',
+      body: JSON.stringify(data),
+    }
+  );
+  const result = await response.json();
+  return result;
+}
 
 
 app.post('/api', (request, response) => {
--- public/sketch.js
+++ public/sketch.js
@@ -14,20 +14,17 @@ async function communicate() {
 }
 communicate();
 
-
+askForPicture() ;
 async function askForPicture() {
-  const data = '{"version": "29565a19f1fb04c8d8e9864bc08a22ce0c982398165fa4cdc61d2f2c777a35a0", "input": {"prompt": "female cyborg assimilated by alien fungus, intricate Three-point lighting portrait, by Ching Yeh and Greg Rutkowski, detailed cyberpunk in the style of GitS 1995"}}' ;
-;
-  console.log("Asking... ", data);
-
+  console.log("Communicating ", data);
   const options = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(data),
+    //body: JSON.stringify(data),
   };
-  const db_response = await fetch("/replicate_api", options);
+  const db_response = await fetch("/replicate_api/hey", options);
   const db_json = await db_response.json();
   console.log(db_json);
 }
