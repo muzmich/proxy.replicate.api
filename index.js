492e88173511635f406ca82a6e55dced5ee95852
Glitch (codingtrain-the-weather-here) <none>
2022-09-19 09:02:36 +0000

    ⛳️🚗 Checkpoint ./index.js:48737440/585

--- index.js
+++ index.js
@@ -28,30 +28,31 @@ app.post("/hello", (request, response) => {
 });
 
 app.post("/replicate_api", async (request, response) => {
-  console.log("got request to talk to replicate", request.params);
+
   let data = {
     version: "29565a19f1fb04c8d8e9864bc08a22ce0c982398165fa4cdc61d2f2c777a35a0",
     input: {
-      prompt:
-        "female cyborg assimilated by alien fungus, intricate Three-point lighting portrait, by Ching Yeh and Greg Rutkowski, detailed cyberpunk in the style of GitS 1995",
+      "prompt":
+        request.body.prompt,
     },
   };
 
-  console.log(request.params.prompt + " but hardwired for now");
-  const api_key = process.env.API_KEY;
-  const replicate_url = `'https://api.replicate.com/v1/predictions',`;
+
+  const api_key = process.env.REPLICATE_API_KEY;
+    console.log("token" ,  api_key , data);
+  const replicate_url = 'https://api.replicate.com/v1/predictions';
   const replicate_response = await fetch(
     // 'https://api-inference.huggingface.co/models/EleutherAI/gpt-neo-125M',
     replicate_url,
     {
-      headers: { Authorization: `Bearer ${api_key}` },
+      headers: { Authorization: 'TOKEN ${api_key}' },
       method: "POST",
       body: JSON.stringify(data),
     }
   );
-  const result = await response.json();
-
-  return result;
+  const result = await replicate_response.json();
+ console.log(result);
+ // return result;
   
 });
 
