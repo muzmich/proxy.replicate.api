7d625304fba0ab2995a95ae93697744df0eecab7
Glitch (codingtrain-the-weather-here) <none>
2022-09-20 09:30:14 +0000

    🙇💏 Checkpoint ./public/index.html:48737440/190 ./public/sketch.js:48737440/1553

--- public/sketch.js
+++ public/sketch.js
@@ -1,3 +1,22 @@
+var input = document.createElement("input_prompt");
+input.type = "text";
+document.appendChild(input); // put it into the DOM
+async function askForPicture() {
+  const data = { prompt: "Tall Frog" };
+  console.log("Asking for Picture ", data);
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+  const picture_response = await fetch("/replicate_api/", options);
+  const proxy_said = await picture_response.json();
+  document.getElementById("show_result").src= proxy_said.output[0];
+  console.log("proxy relayed this about picture:", proxy_said.output[0]);
+}
+
 async function communicate() {
   const data = { incoming: "Hello" };
   console.log("Communicating sending ", data);
@@ -15,24 +34,3 @@ async function communicate() {
  //const db_json = await db_response.json();
   //console.log(db_json);
 }
-//communicate();
-askForPicture() ;
-
-//
-
-async function askForPicture() {
-  const data = { prompt: "Tall Frog" };
-  console.log("Asking for Picture ", data);
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  };
-  const picture_response = await fetch("/replicate_api/", options);
-  const proxy_said = await picture_response.json();
-  document.getElementById("show_result").src= proxy_said.output[0];
-  console.log("proxy relayed this about picture:", proxy_said.output[0]);
-}
-
